763ac42b8484423f91c938da6c76fa7b4f6fb20a
hham2003 <132014372+hham2003@users.noreply.github.com>
2024-11-18 22:50:22 -0500

    Add anomalous data generator
    Randomly generates input strings to be read by data reader, each with a 5% chance of being anomalous

---
+++ anomaly-container/generate-overflow-text.py
@@ -0,0 +1,25 @@
+import string
+import random
+
+chars = list(string.ascii_letters)
+for i in range(10):
+    chars.append(str(i))
+    
+entries = []
+for _ in range(100):
+    entry = ""
+    size = int(15*random.random())
+    if random.random() < 0.05:
+        size += int(30*random.random())
+    
+    for _ in range(size):
+        chars_index = int(len(chars)*random.random())
+        entry += chars[chars_index]
+    
+    entries.append(entry)
+
+
+    
+with open("/Users/henryham/Desktop/f24/Capstone/Docker/testing/overflow-program/data.txt", "w") as file:
+    file.writelines([entry + "\n" for entry in entries])
+    
